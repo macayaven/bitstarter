8a5d2fa73fafcecbd63f709deb5e7439a8386647
Carlos Crespo <macayaven@gmail.com>
2013-07-14 13:41:31 +0000

    add index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var fileContent = fs.readFileSync('index.html', null);	
+	response.send(fileContent.toString(null, null, null));
 });
 
 var port = process.env.PORT || 5000;
